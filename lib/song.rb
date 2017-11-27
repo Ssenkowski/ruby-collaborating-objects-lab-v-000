b20065105761962241b05fecc7e8c967862879c9
Steven Senkowski <scsenkowski@gmail.com>
2017-11-27 21:10:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,4 +11,9 @@ class Song
     new_song.artist.name = artist
     new_song
   end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 end
