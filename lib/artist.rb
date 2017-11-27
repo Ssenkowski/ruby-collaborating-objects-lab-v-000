2cc1e1bdaaeab6e826153abcdc7a5a6e3ef5138e
Steven Senkowski <scsenkowski@gmail.com>
2017-11-27 21:04:57 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,28 +4,22 @@ class Artist
   @@all = []
 
   def initialize(name)
-    @name = name
-    @songs = []
-  end
+  @name = name
+  @songs = []
+end
 
-  def add_song(song)
-    @song = song
-    @songs << @song
-  end
+def self.all
+  @@all    
+end
 
-  def save
-    @artist = self.name
-    @@all << @artist
-  end
+def add_song(song)
+  @songs << song
+end
 
-  def self.find_or_create_by_name(artist)
-    if @@all.find(artist) == nil
-      self.new
-    else @@all.find(artist)
-    end
-  end
+def self.find_or_create_by_name(name)
+  self.find(name) ? self.find(name) : self.create(name)
+end
 
-  def print_songs
-    print @@all
-  end
+def self.find(name)
+  self.all.find {|artist| artist.name == name }
 end
